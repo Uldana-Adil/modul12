992d70ea8524049ef08c117b46db644ae20f5c40
Uldana-Adil <123613496+Uldana-Adil@users.noreply.github.com>
2023-11-26 03:26:01 +0600

    Add files via upload

---
+++ M_12hw/M_12hw/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_12hw
+{
+    public abstract class Car
+    {
+        public string CarType { get; protected set; }
+        public int Speed { get; protected set; }
+        public int Position { get; protected set; }
+
+        public event EventHandler<string> FinishEvent;
+
+        public Car(string carType)
+        {
+            CarType = carType;
+            Position = 0;
+        }
+
+        public void StartRace()
+        {
+            Console.WriteLine($"{CarType} is starting the race!");
+            Random random = new Random();
+            Speed = random.Next(5, 20);
+        }
+
+        public void Move()
+        {
+            Position += Speed;
+            if (Position >= 100)
+            {
+                OnFinish();
+            }
+        }
+
+        protected virtual void OnFinish()
+        {
+            FinishEvent?.Invoke(this, $"{CarType} finished the race!");
+        }
+    }
+
+    public class SportsCar : Car
+    {
+        public SportsCar() : base("Sports Car") { }
+    }
+
+    public class PassengerCar : Car
+    {
+        public PassengerCar() : base("Passenger Car") { }
+    }
+
+    public class Truck : Car
+    {
+        public Truck() : base("Truck") { }
+    }
+
+    public class Bus : Car
+    {
+        public Bus() : base("Bus") { }
+    }
+
+
+}
---
+++ M_12hw/M_12hw/RaceGame.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace M_12hw
+{
+    public class RaceGame
+    {
+        public delegate void StartRaceDelegate();
+        public event StartRaceDelegate StartRaceEvent;
+
+        public RaceGame()
+        {
+            StartRaceEvent += StartRace;
+        }
+
+        public void StartRace()
+        {
+            SportsCar sportsCar = new SportsCar();
+            PassengerCar passengerCar = new PassengerCar();
+            Truck truck = new Truck();
+            Bus bus = new Bus();
+
+            sportsCar.FinishEvent += FinishHandler;
+            passengerCar.FinishEvent += FinishHandler;
+            truck.FinishEvent += FinishHandler;
+            bus.FinishEvent += FinishHandler;
+
+            Console.WriteLine("Race Started!");
+
+            sportsCar.StartRace();
+            passengerCar.StartRace();
+            truck.StartRace();
+            bus.StartRace();
+
+            while (true)
+            {
+                sportsCar.Move();
+                passengerCar.Move();
+                truck.Move();
+                bus.Move();
+
+                Console.WriteLine($"Sports Car: {sportsCar.Position} | Passenger Car: {passengerCar.Position} | Truck: {truck.Position} | Bus: {bus.Position}");
+
+                Thread.Sleep(100);
+
+                if (sportsCar.Position >= 100 || passengerCar.Position >= 100 || truck.Position >= 100 || bus.Position >= 100)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void FinishHandler(object sender, string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("Race Finished!");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            RaceGame raceGame = new RaceGame();
+            raceGame.StartRaceEvent.Invoke();
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
